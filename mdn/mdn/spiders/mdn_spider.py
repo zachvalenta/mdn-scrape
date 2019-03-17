b051af312eb56c50da42c1e8da1e582ac7c9f219
zachvalenta <zjv5002@gmail.com>
2019-03-17 13:25:15 -0400

    namedtuple for link and text

--- mdn/mdn/spiders/mdn_spider.py
+++ mdn/mdn/spiders/mdn_spider.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import re
 
 from loguru import logger
@@ -11,6 +12,7 @@ class MDNSpider(scrapy.Spider):
     name = 'mdn_spider'
     selector = '#Keyword_index + .blockIndicator + div a'
     strip_from_link = '/en-US/docs/Web/CSS'
+    css_kw = namedtuple('css_kw', 'link text')
 
     def start_requests(self):
         logger.debug('making request')
@@ -22,9 +24,9 @@ class MDNSpider(scrapy.Spider):
         logger.debug('parsing request')
         els = response.css(self.selector)
         for el in els:
-            el = el.extract()
-            text = remove_tags(el)
-            logger.debug('text {}', text)
-            link = re.findall(r'\"(.+?)\"', remove_tags(el, keep='a'))
-            link = self.mdn_base_url + link[0].replace(self.strip_from_link, '')
-            logger.debug('link {}', link)
+            el_dirty = el.extract()
+            text = remove_tags(el_dirty)
+            link_dirty = re.findall(r'\"(.+?)\"', remove_tags(el_dirty, keep='a'))
+            link = self.mdn_base_url + link_dirty[0].replace(self.strip_from_link, '')
+            kw = self.css_kw(link, text)
+            logger.debug('link {} text {}'.format(kw.link, kw.text))
