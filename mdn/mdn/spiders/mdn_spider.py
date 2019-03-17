19c993e5ee39e3436594d8bad7f55a3b049a5778
zachvalenta <zjv5002@gmail.com>
2019-03-17 08:24:58 -0400

    refactor for loguru

--- mdn/mdn/spiders/mdn_spider.py
+++ mdn/mdn/spiders/mdn_spider.py
@@ -1,3 +1,4 @@
+from loguru import logger
 import scrapy
 from w3lib.html import remove_tags
 
@@ -5,15 +6,18 @@ from w3lib.html import remove_tags
 class MDNSPider(scrapy.Spider):
 
     name = 'mdn_spider'
+    selector = '#Keyword_index + .blockIndicator + div code'
 
     def start_requests(self):
 
+        logger.debug('making request')
         urls = ['https://developer.mozilla.org/en-US/docs/Web/CSS/Reference']
-
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        items = response.css('#Keyword_index + .blockIndicator + div code')
-        for item in items:
-            print(remove_tags(item.extract()))
+        logger.debug('parsing request')
+        css_kw = response.css(self.selector)
+        for kw in css_kw:
+            clean_kw = remove_tags(kw.extract())
+            logger.debug('css kw {}', clean_kw)
