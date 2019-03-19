55bd87ecbdc92ab720bff4fe63037cb4d32f76db
zachvalenta <zjv5002@gmail.com>
2019-03-19 05:25:53 -0400

    swap in context manager

--- mdn/mdn/spiders/mdn_spider.py
+++ mdn/mdn/spiders/mdn_spider.py
@@ -29,6 +29,5 @@ class MDNSpider(scrapy.Spider):
             link_dirty = re.findall(r'\"(.+?)\"', remove_tags(el_dirty, keep='a'))
             link = self.mdn_base_url + link_dirty[0].replace(self.strip_from_link, '')
             self.all_kw.append(dict(link=link, text=text))
-        data = open('data.json', 'w')
-        data.write(json.dumps(self.all_kw, indent=4))
-        data.close()
+        with open('data.json', 'w') as f:
+            f.write(json.dumps(self.all_kw, indent=4))
